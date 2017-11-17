6105571014e98a082a31dcfb4b0674055a5eb08c
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 19:59:47 -0600

    Se ingresa un numero cualquiera y se muestra el resultado del numeroelevado a una potencia dependiendo de este, entre la multiplicacion de los primos correspondientes

---
+++ Particion/Particion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Particion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[1];
+            Console.WriteLine("Igresa un numero de 9 digitos");
+            int num = n[1];
+
+            int c = 0;
+            for (int m = num; m > 0;)
+            {
+                m = m / 10;
+                c++;
+            }
+
+            if (c == 9)
+            {
+                Console.Write("[");
+                for (int nm = 0; nm >= 0; nm++)
+                {
+                    Console.Write("{0},", n[nm]);
+                }
+                Console.WriteLine("]");
+            }
+            Console.ReadKey();
+        }
+    }
+}
